979a5126c2aef2e9c2ee5c176a5b74aa64d8d87c
DTXingFeng <92532952+DTXingFeng@users.noreply.github.com>
2023-10-08 21:31:10 +0800

    bate

--- src/main/java/xyz/xingfeng/SteamBalanceServer/Initialization/Inspect.java
+++ src/main/java/xyz/xingfeng/SteamBalanceServer/Initialization/Inspect.java
@@ -2,10 +2,58 @@ package xyz.xingfeng.SteamBalanceServer.Initialization;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.File;
+import java.io.IOException;
+
 @Slf4j
 public class Inspect {
     public Inspect(){
-        log.info("检查'cookie'文件");
-
+        log.info("检查'cookie'文件是否存在");
+        File file = new File("config/cookie");
+        if (!file.exists()){
+            //不存在
+            log.info("文件不存在，进行初始化");
+            try {
+                if (file.createNewFile()){
+                    log.info("创建成功");
+                }else {
+                    log.error("创建失败");
+                }
+            } catch (IOException e) {
+                log.error("创建文件”cookie”时发生了未知错误”");
+                throw new RuntimeException(e);
+            }
+        }
+        log.info("检查“dotaItem.json”文件与”csItem.json“是否存在");
+        file = new File("config/dotaItem.json");
+        if (!file.exists()){
+            //不存在
+            log.info("文件不存在，进行初始化");
+            try {
+                if (file.createNewFile()){
+                    log.info("创建成功");
+                }else {
+                    log.error("创建失败");
+                }
+            } catch (IOException e) {
+                log.error("创建文件”dotaItem.json“时发生了未知错误”");
+                throw new RuntimeException(e);
+            }
+        }
+        file = new File("config/csItem.json");
+        if (!file.exists()){
+            //不存在
+            log.info("文件不存在，进行初始化");
+            try {
+                if (file.createNewFile()){
+                    log.info("创建成功");
+                }else {
+                    log.error("创建失败");
+                }
+            } catch (IOException e) {
+                log.error("创建文件”csItem.json“时发生了未知错误”");
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
--- src/main/java/xyz/xingfeng/SteamBalanceServer/SteamBalanceServerApplication.java
+++ src/main/java/xyz/xingfeng/SteamBalanceServer/SteamBalanceServerApplication.java
@@ -3,6 +3,7 @@ package xyz.xingfeng.SteamBalanceServer;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import xyz.xingfeng.SteamBalanceServer.Initialization.Inspect;
 
 @Slf4j
 @SpringBootApplication
@@ -11,7 +12,7 @@ public class SteamBalanceServerApplication {
 	public static void main(String[] args) {
 		//开始初始化程序
 		log.info("检查必要文件");
-
+		new Inspect();
 		SpringApplication.run(SteamBalanceServerApplication.class, args);
 	}
 
